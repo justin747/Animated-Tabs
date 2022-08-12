394d7a9ad3b2556513bb24933811303a30f585e4
justin747 <justin747@gmail.com>
2022-08-11 20:23:39 -0700

    Initial Commit

---
+++ Animated Tabs/Animated_TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_TabsApp.swift
+//  Animated Tabs
+//
+//  Created by Justin on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Animated_TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated Tabs/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Animated Tabs
+//
+//  Created by Justin on 8/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
