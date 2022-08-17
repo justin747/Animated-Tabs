75a305585e561dde73cc4617eb8f1e00cf758079
justin747 <justin747@gmail.com>
2022-08-16 22:13:28 -0700

    Added Tab Model
    Added Tab Model

---
+++ Animated Tabs/Model/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  Animated Tabs
+//
+//  Created by Justin on 8/16/22.
+//
+
+import SwiftUI
+
+enum Tab: String,CaseIterable {
+    
+    case home = "Home"
+    case chat = "Chat"
+    case notifications = "Notifications"
+    case saved = "Saved"
+    case account = "Account"
+    
+}
